db754234e6f43fcd9d437db5b0d8a824c18e8898
Kirill <104680459+L1irik259@users.noreply.github.com>
2026-03-17 16:16:01 +0300

    Update proto

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/L1irik259/TestForOzon/internal/transport/proto/github.com/L1irik259/TestForOzon/transport/genetation/go/v1"
+	transport "github.com/L1irik259/TestForOzon/internal/transport/service"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	server := &transport.Server{}
+	pb.RegisterOzonServiceServer(grpcServer, server)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
